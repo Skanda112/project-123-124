cdee3876f01240d8e51709b95d4a5db4633f6cc2
Skanda112 <72615563+Skanda112@users.noreply.github.com>
2021-06-03 20:31:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ leftWristX = 0;
 
 function setup() {
     video = createCapture(VIDEO);
-    video.size(550, 500);
+    video.size(550, 399);
   
-    canvas = createCanvas(550, 550);
-    canvas.position(560,150);
+    canvas = createCanvas(550, 399);
+    canvas.position(580, 150);
   
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
@@ -26,6 +26,26 @@ function modelLoaded() {
     if(results.length > 0)
     {
       console.log(results);
+      noseX = results[0].pose.nose.x;
+      noseY = results[0].pose.nose.y;
+      console.log("noseX = " + noseX +" noseY = " + noseY);
+  
+      leftWristX = results[0].pose.leftWrist.x;
+      rightWristX = results[0].pose.rightWrist.x;
+      difference = floor(leftWristX - rightWristX);
+  
+      console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
      
     }
+  }
+
+  
+function draw() {
+  background('#ffff00');
+  textSize(difference);
+text('IRON MAN', 50, 300);
+fill(0, 0, 0);
+  
+    document.getElementById("text_side").innerHTML = "Width And Height of a text will be = " + difference +"px";
+
   }
